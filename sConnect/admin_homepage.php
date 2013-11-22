d8012f289297e85f557a73b68d63b087684b34ee
jjew <jonathanjewv@gmail.com>
2013-11-22 12:56:33 -0800

    Basic delete posts functionality added

--- sConnect/admin_homepage.php
+++ sConnect/admin_homepage.php
@@ -3,7 +3,12 @@
 // Start the session
     session_start();
 
-    ?>
+// Redirect if not logged in
+	if( !isset( $_SESSION['admin'] )){
+		header('Location: login_admin.php');
+	}
+	
+?>
 
 <!doctype html>
 <html>
@@ -24,6 +29,9 @@
 </div>
 
 <div id="nav">
-<a href="admin_stats.php">Stats</a></br>
-<a href="logout_admin.php">Logout</a></br>
+
+<a href="admin_stats.php">Stats</a><br>
+<a href="admin_delete.php">Delete Post</a><br>
+<br>
+<a href="logout_admin.php">Logout</a><br>
 </div>
